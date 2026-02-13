3765c98359a4d260e5c353ffd4ec04bcc08ec68e
Ratatoskr589 <86021885+Ratatoskr589@users.noreply.github.com>
2026-02-13 13:29:10 -0700

    Update pub_20251111_FRHoF.js
    Added a nameshort for Helpful Homunculi since it gets cut off otherwise.

--- pub_20251111_FRHoF.js
+++ pub_20251111_FRHoF.js
@@ -3166,7 +3166,8 @@ SpellsList["death armor"] = {
     ],
 };
 SpellsList["deryan's helpful homunculi"] = {
-    name: "Deryan's Helpful Homunculi",
+    name: "Deryan's Helpful Homunculi
+	nameShort: "D's Helpful Homunculi",
     classes: ["cleric", "wizard"],
     source: [
         ["FRHoF", 143]
