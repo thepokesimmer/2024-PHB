5933b147376c62fbf5f453be8b9c63eb1db7dfaf
thepokesimmer <140215008+thepokesimmer@users.noreply.github.com>
2026-05-07 19:43:44 -0500

    Update pub_20240917_PHB.js
    Updated Otto's Irresistible Dance adding Wis save.

--- pub_20240917_PHB.js
+++ pub_20240917_PHB.js
@@ -21213,6 +21213,7 @@ legacySpellRefactor("otto's irresistible dance", {
     range: "30 ft",
     components: "V",
     duration: "Conc, 1 min",
+	save: "Wis",
     description: "1 crea pass: dance to next EoT; fail: Charmed, no move, dis. atk/Dex saves, adv. atk vs it; 1 a save to end",
     descriptionFull: "One creature that you can see within range must make a Wisdom saving throw. On a successful save, The target dances comically until the end of its next turn, during which it must spend all its movement to dance in place." + "\n   " + "On a failed save, the target has the Charmed condition for the duration. While Charmed, the target dances comically, must use all its movement to dance in place, and has Disadvantage on Dexterity saving throws and attack rolls, and other creatures have Advantage on attack rolls against it. On each of its turns, the target can take an action to collect itself and repeat the save, ending the spell on itself on a success."
 });
